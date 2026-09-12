7b6277f9d5e325cbba0ffebab3f218a0bf7e33be
ickymarques <icky337@gmail.com>
2026-09-12 02:18:47 -0300

    Limpa cache após retorno à versão estável

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-financeiro-v5-5-2';
+const CACHE='meu-financeiro-stable-20260912';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
