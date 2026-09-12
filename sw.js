d402ebf01664de7b2928223fa6963569e521b12a
ickymarques <icky337@gmail.com>
2026-09-12 00:09:14 -0300

    Adiciona service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='meu-financeiro-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))));
