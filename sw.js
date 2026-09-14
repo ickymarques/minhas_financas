be498237751c5a793b3f4afcbbbd933be44df170
ickymarques <icky337@gmail.com>
2026-09-14 20:58:47 -0300

    Atualiza cache após melhorias do importador PDF

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-financeiro-v5-9-2';
+const CACHE='meu-financeiro-v5-9-3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./invoice-import.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
